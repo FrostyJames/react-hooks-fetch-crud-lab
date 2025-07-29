39c635df630749b7c8e150cd2c5015bf58a54f7f
james <ivanlavan773@gmail.com>
2025-07-29 12:25:11 +0300

    updated code

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,32 +1,64 @@
-function handleSubmit(event) {
-  event.preventDefault();
+import React, { useState } from "react";
 
-  const newQuestion = {
-    prompt: formData.prompt,
-    answers: [
-      formData.answer1,
-      formData.answer2,
-      formData.answer3,
-      formData.answer4,
-    ],
-    correctIndex: parseInt(formData.correctIndex),
-  };
+function QuestionForm({ questions, setQuestions }) {
+  const [formData, setFormData] = useState({
+    prompt: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    correctIndex: 0,
+  });
 
-  fetch("http://localhost:4000/questions", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newQuestion),
-  })
-    .then((res) => res.json())
-    .then((addedQuestion) => {
-      props.setQuestions([...props.questions, addedQuestion]);
-      setFormData({
-        prompt: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctIndex: 0,
-      });
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
     });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((res) => res.json())
+      .then((addedQuestion) => {
+        setQuestions([...questions, addedQuestion]);
+        setFormData({
+          prompt: "",
+          answer1: "",
+          answer2: "",
+          answer3: "",
+          answer4: "",
+          correctIndex: 0,
+        });
+      });
+  }
+
+  return (
+    <section>
+      <h1>New Question</h1>
+      <form onSubmit={handleSubmit}>
+        {/* form fields */}
+        <button type="submit">Add Question</button>
+      </form>
+    </section>
+  );
 }
+
+export default QuestionForm;
