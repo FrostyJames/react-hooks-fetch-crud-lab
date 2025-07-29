e2962911d113e6320a668352b76e907b79b1f934
james <ivanlavan773@gmail.com>
2025-07-29 12:28:24 +0300

    updated code

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -11,9 +11,11 @@ function QuestionForm({ questions, setQuestions }) {
   });
 
   function handleChange(event) {
+    const { name, value } = event.target;
+
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value,
+      [name]: name === "correctIndex" ? parseInt(value) : value,
     });
   }
 
@@ -28,7 +30,7 @@ function QuestionForm({ questions, setQuestions }) {
         formData.answer3,
         formData.answer4,
       ],
-      correctIndex: parseInt(formData.correctIndex),
+      correctIndex: formData.correctIndex,
     };
 
     fetch("http://localhost:4000/questions", {
@@ -54,7 +56,70 @@ function QuestionForm({ questions, setQuestions }) {
     <section>
       <h1>New Question</h1>
       <form onSubmit={handleSubmit}>
-        {/* form fields */}
+        <label>
+          Prompt:
+          <input
+            type="text"
+            name="prompt"
+            value={formData.prompt}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Answer 1:
+          <input
+            type="text"
+            name="answer1"
+            value={formData.answer1}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Answer 2:
+          <input
+            type="text"
+            name="answer2"
+            value={formData.answer2}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Answer 3:
+          <input
+            type="text"
+            name="answer3"
+            value={formData.answer3}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Answer 4:
+          <input
+            type="text"
+            name="answer4"
+            value={formData.answer4}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Correct Answer:
+          <select
+            name="correctIndex"
+            value={formData.correctIndex}
+            onChange={handleChange}
+          >
+            <option value="0">Answer 1</option>
+            <option value="1">Answer 2</option>
+            <option value="2">Answer 3</option>
+            <option value="3">Answer 4</option>
+          </select>
+        </label>
+
         <button type="submit">Add Question</button>
       </form>
     </section>
